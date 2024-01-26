0fe6daa8286f7bd453a3b3efb7d2c8d248c52649
JerryWang0122 <school832@gmail.com>
2024-01-27 00:30:20 +0800

    feat: 調整交易紀錄新增及刪除邏輯，使其同步更新股利的sharesHold

--- services/transaction-services.js
+++ services/transaction-services.js
@@ -1,12 +1,25 @@
-const { Stock, User, Transaction } = require('../models')
+const { Stock, User, Transaction, Dividend } = require('../models')
 const { isValidDateFormat } = require('../helpers/input-helper')
 const { checkSharesHold } = require('../helpers/transaction-helper')
+const { Op } = require('sequelize')
+
+const path = require('path')
+const env = process.env.NODE_ENV || 'development'
+const config = require(path.resolve(__dirname, '../config/config.json'))[env]
+const Sequelize = require('sequelize')
+let sequelize
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
+}
 
 const transServices = {
   addTransaction: async (req, cb) => {
     const { transDate, isBuy, quantity, pricePerUnit, fee, note, stockId } = req.body
     const userId = req.user.id
 
+    const t = await sequelize.transaction()
     try {
       // check Date Format
       if (!isValidDateFormat(transDate)) throw new Error('Invalid Date Format! Should be YYYY-MM-DD')
@@ -23,12 +36,31 @@ const transServices = {
       // 如果要賣，先檢查庫存
       if (!isBuy && await checkSharesHold(userId, stockId, transDate, quantity)) throw new Error('該操作將導致股票庫存不足，請確認交易紀錄正確性！')
 
-      // 建立交易紀錄
-      // 之後需要考慮股利
-      const transaction = await Transaction.create({ transDate, isBuy, quantity, pricePerUnit, fee, note, userId, stockId })
+      // 建立交易紀錄，同時更新影響到的股利sharesHold
+      const [transaction, _] = await Promise.all([
+        Transaction.create(
+          { transDate, isBuy, quantity, pricePerUnit, fee, note, userId, stockId },
+          { transaction: t }
+        ),
+        Dividend.increment(
+          { sharesHold: isBuy ? quantity : -quantity },
+          {
+            where: {
+              userId,
+              stockId,
+              dividendDate: {
+                [Op.gt]: transDate
+              }
+            }
+          },
+          { transaction: t }
+        )
+      ])
+      await t.commit()
 
       return cb(null, { transaction })
     } catch (err) {
+      await t.rollback()
       return cb(err)
     }
   },
@@ -44,6 +76,7 @@ const transServices = {
       .catch(err => cb(err))
   },
   deleteTransaction: async (req, cb) => {
+    const t = await sequelize.transaction()
     try {
       const transId = req.params.tid
       const transaction = await Transaction.findByPk(transId)
@@ -54,9 +87,28 @@ const transServices = {
       // 如果要刪掉的是買的，要檢查庫存
       if (transaction.isBuy && await checkSharesHold(req.user.id, transaction.stockId, transaction.transDate, transaction.quantity)) throw new Error('該操作將導致股票庫存不足，請確認交易紀錄正確性！')
 
-      const deletedTransaction = await transaction.destroy()
+      // 刪掉交易紀錄，同時更新股利的sharesHold
+      const [deletedTransaction, _] = await Promise.all([
+        transaction.destroy({ transaction: t }),
+        Dividend.increment(
+          { sharesHold: !transaction.isBuy ? transaction.quantity : -transaction.quantity },
+          {
+            where: {
+              userId: req.user.id,
+              stockId: transaction.stockId,
+              dividendDate: {
+                [Op.gt]: transaction.transDate
+              }
+            }
+          },
+          { transaction: t }
+        )
+      ])
+      await t.commit()
+
       cb(null, { deletedTransaction })
     } catch (err) {
+      await t.rollback()
       cb(err)
     }
   }
